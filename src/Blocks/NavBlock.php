6d55cffedbee93912e9b0ad27028a5652203d92e
jesspinkman <serge.goncalves@gmail.com>
2021-05-27 10:13:15 +0800

    navblock less opinonated

--- src/Blocks/NavBlock.php
+++ src/Blocks/NavBlock.php
@@ -5,6 +5,8 @@ namespace BSPure\Blocks;
 use BSPure\BSPure;
 use BSPure\Components\BSBaseComponent;
 use BSPure\Components\NavBar;
+use Closure;
+use Stringable;
 
 class NavBlock extends NavBar
 {
@@ -15,18 +17,22 @@ class NavBlock extends NavBar
     public function __construct()
     {
         parent::__construct(true);
-        $this->variant('dark')
-            ->expand('md')
-            ->bg('dark')
-            ->shadow('sm')
-            ->sticky()
-            ->___(
-                $this->brand = BSPure::navBarBrand()->href('/'),
-                BSPure::navBarToggler('#nav-menu'),
-                BSPure::navBarCollapse('nav-menu')(
-                    $this->menu = BSPure::navBarNav()
-                        ->m(null, 'lg-5', null, 'auto')
-                )
-            );
+        $this(
+            $this->brand = BSPure::navBarBrand()->href('/'),
+            BSPure::navBarToggler('#nav-menu'),
+            BSPure::navBarCollapse('nav-menu')(
+                $this->menu = BSPure::navBarNav()
+            )
+        );
+    }
+
+    public function brand(Stringable|string|array|Closure|null $request): static
+    {
+        return $this->pureAccess('brand', $request);
+    }
+
+    public function menu(Stringable|string|array|Closure|null $request): static
+    {
+        return $this->pureAccess('menu', $request);
     }
 }
